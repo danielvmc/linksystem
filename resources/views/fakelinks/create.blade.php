b3336b72abaf430c125db55c3291879eb8f4996b
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-10-05 10:48:02 +0700

    fix

--- resources/views/fakelinks/create.blade.php
+++ resources/views/fakelinks/create.blade.php
@@ -16,9 +16,64 @@
             <div class="form-group">
               <label for="fake_domain">Chọn website tuỳ vào nước spam:</label>
               <select name="fake_domain" id="fake_domain" class="form-control">
-                @foreach ($domains as $domain)
+                {{-- @foreach ($domains as $domain)
                   <option value="{{ $domain->name }}">{{ $domain->name }}</option>
-                @endforeach
+                @endforeach --}}
+
+    <optgroup label="Việt Nam">
+        <option value="tuoitrehot.info">tuoitrehot.info</option>
+        <option value="gioitrevietnam.info">gioitrevietnam.info</option>
+        <option value="danvietnam.info">danvietnam.info</option>
+        <option value="nguoivietbonphuong.info">nguoivietbonphuong.info</option>
+        <option value="baonguoiviet.info">baonguoiviet.info</option>
+        <option value="baonhandan.info">baonhandan.info</option>
+        <option value="baophunuviet.info">baophunuviet.info</option>
+        <option value="vzexpress.info">vzexpress.info</option>
+        <option value="vmexpress.info">vmexpress.info</option>
+        <option value="congtin.info">congtin.info</option>
+        <option value="docbaophapluat.info">docbaophapluat.info</option>
+        <option value="tokhoe.info">tokhoe.info</option>
+        <option value="tamsueva.info">tamsueva.info</option>
+    </optgroup>
+    <optgroup label="Indonesia">
+        <option value="medan.tribunnews-indo.info">medan.tribunnews-indo.info</option>
+        <option value="jambi.tribunnews-indo.info">jambi.tribunnews-indo.info</option>
+        <option value="style.tribunnews-indo.info">style.tribunnews-indo.info</option>
+        <option value="solo.tribunnews-indo.info">solo.tribunnews-indo.info</option>
+        <option value="bogor.tribunnews-indo.info">bogor.tribunnews-indo.info</option>
+        <option value="wartakota.tribunnews-indo.info">wartakota.tribunnews-indo.info</option>
+        <option value="superball.tribunnews-indo.info">superball.tribunnews-indo.info</option>
+        <option value="jatim.tribunnews-indo.info">jatim.tribunnews-indo.info</option>
+        <option value="blog.tribunnews-indo.info">blog.tribunnews-indo.info</option>
+        <option value="jabar.tribunnews-indo.info">jabar.tribunnews-indo.info</option>
+        <option value="palemband.tribunnews-indo.info">palemband.tribunnews-indo.info</option>
+        <option value="wow.tribunnews-indo.info">wow.tribunnews-indo.info</option>
+        <option value="medan.thetribunnews.info">medan.thetribunnews.info</option>
+        <option value="jambi.thetribunnews.info">jambi.thetribunnews.info</option>
+        <option value="style.thetribunnews.info">style.thetribunnews.info</option>
+        <option value="solo.thetribunnews.info">solo.thetribunnews.info</option>
+        <option value="bogor.thetribunnews.info">bogor.thetribunnews.info</option>
+        <option value="wartakota.thetribunnews.info">wartakota.thetribunnews.info</option>
+        <option value="jatim.thetribunnews.info">jatim.thetribunnews.info</option>
+        <option value="blog.thetribunnews.info">blog.thetribunnews.info</option>
+        <option value="superball.thetribunnews.info">superball.thetribunnews.info</option>
+        <option value="jabar.thetribunnews.info">jabar.thetribunnews.info</option>
+        <option value="palembang.thetribunnews.info">palembang.thetribunnews.info</option>
+        <option value="wow.thetribunnews.info">wow.thetribunnews.info</option>
+    </optgroup>
+    <optgroup label="Philippines">
+        <option value="philippinesdaily.info">philippinesdaily.info</option>
+        <option value="newsbalita.info">newsbalita.info</option>
+        <option value="manilatimesph.info">manilatimesph.info</option>
+        <option value="sunstarph.info">sunstarph.info</option>
+        <option value="gmanews.info">gmanews.info</option>
+        <option value="inquirernews.info">inquirernews.info</option>
+        <option value="abscbn-news.info">abscbn-news.info</option>
+        <option value="trendingnewsportals.info">trendingnewsportals.info</option>
+        <option value="trendingnewsportalph.info">trendingnewsportalph.info</option>
+        <option value="abscbnph.info">abscbnph.info</option>
+        <option value="rapplerph.info">rapplerph.info</option>
+    </optgroup>
               </select>
             </div>
 
