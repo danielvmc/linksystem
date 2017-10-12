1614f08a8e4f30136800af2c3df6f0f4337ebaf5
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-10-12 12:38:58 +0700

    fix

--- resources/views/fakelinks/show.blade.php
+++ resources/views/fakelinks/show.blade.php
@@ -2,11 +2,13 @@
 <html lang="en">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>{!! $link->title !!}</title>
     <meta http-equiv="X-UA-Compatible" content="IE=100">
-    <meta property="fb:app_id" content="1547540628876392">
+    {{-- <meta property="fb:app_id" content="1547540628876392"> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     <meta http-equiv="REFRESH" content="1800">
-    <meta content="article" property="og:type">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="article">
     <meta content="news" itemprop="genre" name="medium">
     <meta content="vi-VN" itemprop="inLanguage">
     <meta content="Tin nhanh VnExpress" property="og:site_name">
@@ -14,7 +16,7 @@
     <meta property="og:title" content='{!! $link->title !!}' />
     <meta property="og:description" content="{!! $link->description !!}" />
     <meta property="og:image" content="{{ $link->img }}" />
-    <title>{!! $link->title !!}</title>
+    <meta property="fb:pages" content="776817119094980">
 </head>
 <body>
     <p>Loading...</p>
