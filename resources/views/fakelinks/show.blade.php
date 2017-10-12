5e2abdaabab3aba592182db7e78683f50ab767ba
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-10-12 12:30:07 +0700

    fix

--- resources/views/fakelinks/show.blade.php
+++ resources/views/fakelinks/show.blade.php
@@ -5,10 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=100">
     <meta property="fb:app_id" content="1547540628876392">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    <meta http-equiv="REFRESH" content="1800">
     <meta content="article" property="og:type">
+    <meta content="news" itemprop="genre" name="medium">
+    <meta content="vi-VN" itemprop="inLanguage">
     <meta content="Tin nhanh VnExpress" property="og:site_name">
     <meta charset="UTF-8">
-    <meta property="og:title" content="{!! $link->title !!}" />
+    <meta property="og:title" content='{!! $link->title !!}' />
     <meta property="og:description" content="{!! $link->description !!}" />
     <meta property="og:image" content="{{ $link->img }}" />
     <title>{!! $link->title !!}</title>
