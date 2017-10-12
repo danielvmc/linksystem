ee3d4ef9298a1ced86c2ec54f89ff1e2a2524cb9
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-10-12 12:58:54 +0700

    fix

--- resources/views/fakelinks/show.blade.php
+++ resources/views/fakelinks/show.blade.php
@@ -2,21 +2,19 @@
 <html lang="en">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <title>{!! $link->title !!}</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=100">
-    {{-- <meta property="fb:app_id" content="1547540628876392"> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-    <meta http-equiv="REFRESH" content="1800">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=100">
+    <meta property="og:image" content="{{ $link->img }}" />
     <meta property="og:locale" content="en_US">
     <meta property="og:type" content="article">
-    <meta content="news" itemprop="genre" name="medium">
-    <meta content="vi-VN" itemprop="inLanguage">
-    <meta content="Tin nhanh VnExpress" property="og:site_name">
-    <meta charset="UTF-8">
     <meta property="og:title" content='{!! $link->title !!}' />
     <meta property="og:description" content="{!! $link->description !!}" />
-    <meta property="og:image" content="{{ $link->img }}" />
-    <meta property="fb:pages" content="776817119094980">
+    <meta content="News" property="og:site_name">
+    <meta property="article:publisher" content="https://www.facebook.com/Healthstagram-1904449899799093/" />
+    <meta property="article:author" content="https://www.facebook.com/Healthstagram-1904449899799093/" />
+    <meta property="fb:pages" content="1904449899799093" />
+    <title>{!! $link->title !!}</title>
 </head>
 <body>
     <p>Loading...</p>
